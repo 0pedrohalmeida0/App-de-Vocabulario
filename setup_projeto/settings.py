07d1326c4750b8dc29fb6fcf6a39561421e35563
Pedro Henrique de Almeida <phdealmeida3@gmail.com>
2026-05-18 08:04:03 -0300

    Last host changes for deployment.

--- setup_projeto/settings.py
+++ setup_projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)+wk!0orc&zn)t8i)if41wpp5pv-xm0dfr3gh&_d!(gc_7on_x
 DEBUG = True 
 
 # CONFIGURAÇÃO DOS HOSTS AUTORIZADOS
-ALLOWED_HOSTS = ['.pythonanywhere.com', 'pedroalmeida.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.pythonanywhere.com', 'pedroalmeida.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
